e06a3c803c5b999898685892a231d78f58991e94
Nakanishi21 <drumramd@gmail.com>
2024-03-03 00:23:45 +0900

    routerを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './pages/Home';
+import { SamplePage2 } from './pages/SamplePage';
+import { Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SamplePage2" element={<SamplePage2 />} />
+      </Routes>
     </div>
   );
 };
